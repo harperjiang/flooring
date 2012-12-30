c4a0d5e1c7bd332eb9468351e55d0855135207f5
wenjin.hu@gmail.com <wenjin.hu@gmail.com@6e623c21-3d16-59ef-3494-792983218cb8>
2012-12-30 07:01:05 +0000

    Based on each single file Able to send query and get stats dynamically draw the plot on the fly

--- Flooring/WebContent/script/plot.js
+++ Flooring/WebContent/script/plot.js
@@ -1,94 +1,122 @@
 var scenes = new Array(10);
-var scene;
+var stats = new Array(10);
 var index = 0;
-var sceneno = 10; 
+var sceneno = 8; 
+var scene = {};
+var stat = {};
 function init() {
         for (var x = 0; x < sceneno;x++) {
             scenes[x] = new Array(9);
             scenes[x][0] = 0;
+            stats[x] = new Array(9);
+            stats[x][0] = 0;
         }
-        scene["scheme"]="";
-        scene["house"]="";
-        scene["floor"]="";
-        scene["loading"]="";
-        scene["ventilation"]="";
-        scene["particle"]="";
-        scene["resuspension"]="";
         scenelist();
-	//getstats();
     }
     window.onload = init;
-    
-    function choose(form)
-    {
-    	var radios = document[form][form];
-    	for(var i=0; i<radios.length;i++){
-    		if( radios[i].checked) {
-    	    	scene[form] = radios[i].value;
-    		}
-    	}
-    	document.getElementById("parameter").innerHTML = form + " is selected to be " + scene[form];
-    }
-    
+
     //for display button
-    function select() {
-
-        if (index >= sceneno) {
-            document.getElementById("parameter").innerHTML = "Only support " + sceneno + " scenes<br />"
-                                                            + "Please remove some scenes before adding more";
-            return;
-        }
+    function check() {
 
 
-        document.getElementById("parameter").innerHTML = "";
-        if (scene["scheme"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "scheme should be selected";
-            return;
-        }
-        if (scene["house"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "house type should be selected";
-            return;
-        }
-        if (scene["floor"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "floor type should be selected";
-            return;
-        }
-        if (scene["loading"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "floor loading should be selected";
-            return;
-        }
-        if (scene["ventilation"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "ventilation rate should be selected";
-            return;
-        }
-        if (scene["particle"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "particle granularity should be selected";
-            return;
-        }
-        if (scene["resuspension"]=="")
-        {
-            document.getElementById("parameter").innerHTML = "probability of suspension granularity should be selected";
-            return;
-        }
-        
+    	if (index >= sceneno) {
+    		document.getElementById("parameter").innerHTML = "Only support " + sceneno + " scenes<br />" 
+    		                                               + "Please remove some scenes before adding more";
+    		return;
+    	}
+
+    	document.getElementById("parameter").innerHTML = "";
+    	if (document.getElementById("scheme").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "scheme should be selected";
+        	alert("Am I here?");
+        	return;
+    	}
+    	if (document.getElementById("house").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "house type should be selected";
+    		return;
+    	}
+    	if (document.getElementById("floor").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "floor type should be selected";
+    		return;
+    	}
+    	if (document.getElementById("loading").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "floor loading should be selected";
+    		return;
+    	}
+    	if (document.getElementById("ventilation").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "ventilation rate should be selected";
+    		return;
+    	}
+    	if (document.getElementById("particle").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "particle granularity should be selected";
+    		return;
+    	}
+    	if (document.getElementById("resuspension").value=="")
+    	{
+    		document.getElementById("parameter").innerHTML = "probability of suspension granularity should be selected";
+    		return;
+    	}
+    	var i = 0;
+    	while (scenes[i][0]== 1) {
+    		i++;
+    	}
+
+
+    	if (i < scenes.length) {
+
+    		scenes[i][0] = 0;
+    		scenes[i][1] = 0;
+    		scenes[i][2] = document.getElementById("scheme").value;
+    		scenes[i][3] = document.getElementById("house").value;
+    		scenes[i][4] = document.getElementById("floor").value;
+    		scenes[i][5] = document.getElementById("loading").value;
+    		scenes[i][6] = document.getElementById("ventilation").value;
+    		scenes[i][7] = document.getElementById("particle").value;
+    		scenes[i][8] = document.getElementById("resuspension").value;
+
+    		scene.scheme = document.getElementById("scheme").value;
+    		scene.house= document.getElementById("house").value;
+    		scene.floor= document.getElementById("floor").value;
+    		scene.loading= document.getElementById("loading").value;
+    		scene.ventilation = document.getElementById("ventilation").value;
+    		scene.particle= document.getElementById("particle").value;
+    		scene.resuspension = document.getElementById("resuspension").value;
+    		//for debugging scene array+`
+    		alert(JSON.stringify(scene));
+    		var s = "Scene: ";
+    		
+    		for (var x=2; x< scenes[i].length; x++) {
+    			s += scenes[i][x] + "; ";
+    		}
+    		s += "is added<br />";
+
+    		document.getElementById("parameter").innerHTML = "<p>" + s + "</p>";
+            ajaxsend(i);
+
+    	}
+    	else {
+    		document.getElementById("parameter").innerHTML =   "Scenes are selected more than "+ sceneno+"<br />"
+    		+ "Please remove some scenes before adding more";
+    	}
     }
     
     function scenelist() {
 
         var s = new String();
-        s += "<form><select id='scenes' style='width:800px;'>";
+        s += "<form><select id='scenelist' style='width:800px;'>";
         s += "<option value='-1' selected>"+ index + " scenes have been displayed";
         if (index > 0) {
             for (x in scenes) {
                 if (scenes[x][0] == 1) {
                     s += "<option value="+x+">";
-                    for (y in scenes[x]) {
+                    s+= "scene " + x + " : ";
+                    for (var y=2; y< scenes[x].length;y++) {
                         s += scenes[x][y] + ";";
                     }
                 }
@@ -96,25 +124,28 @@ function init() {
             s += "</select></form>";
         }
         s += "<input type='button' id='remove' value='Remove' onClick='sceneremove();'/>";
+        s += "<input type='button' id='download' value='Download' onClick='download();'/>";
         s += "<br />";
         document.getElementById("scenes").innerHTML = s;
-
         if (index == 0) {
             drawaxis();
         } else {
-            drawplot(index);
+        	//alert("It should go here");
+            drawplot();
         }
 
     }
 
     function sceneremove() {
-        var i = document.getElementById("scenes").value;
+        var i = document.getElementById("scenelist").value;
         if (i != "-1") {
             scenes[i][0] = 0;
+            scenes[i][1] = 0;
+            stats[i].valid = 0;
             index--;
             var s = new String();
 
-            for (x in scenes[i]) {
+            for (var x=0; x<scenes[i].length; x++) {
                 s += scenes[i][x] + "; ";
             }
             s += "is removed<br />";
@@ -125,11 +156,70 @@ function init() {
             document.getElementById("parameter").innerHTML = "No scene is selected to remove. Please click the droplist";
         }
         scenelist();
+}
+    
+    function clearplot(){
+    	for (var i=0; i<sceneno;i++) {
+    		scenes[i][0] = 0;
+    		scenes[i][1] = 0;
+    		stats[i].valid = 0;
+    	}
+    	index=0;
+    	//should reset scene and stat too
+    	scenelist();
     }
+    
+    function ajaxsend(idx){
+    	var xmlhttp;
+    	xmlhttp = new XMLHttpRequest();
+    	xmlhttp.onreadystatechange=function()
+    	{
+    	    if(xmlhttp.readyState==4 && xmlhttp.status==200)
+    	    {
+    	    	
+    	    	document.getElementById("parameter").innerHTML="";
+    	    	//document.getElementById("parameter").innerHTML=xmlhttp.responseText;
+    	    	var stat = eval("("+xmlhttp.responseText+")");
+    	    	if(stat.valid==1){
+    	    		document.getElementById("parameter").innerHTML +=  "Scene " + idx + " stats as below:<br />";	    		
+    	    		document.getElementById("parameter").innerHTML +=  "--------record min ------> " + stat.min +"<br />";
+    	    		document.getElementById("parameter").innerHTML +=  "--------record 25% ------> " + stat.p25 +"<br />";
+    	    		document.getElementById("parameter").innerHTML +=  "--------record medium ---> " + stat.medium +"<br />";
+    	    		document.getElementById("parameter").innerHTML +=  "--------record 75% ------> " + stat.p75 +"<br />";
+    	    		document.getElementById("parameter").innerHTML +=  "--------record max ------> " + stat.max +"<br />";
 
-    function drawplot(index) {
+    	    		stats[idx]= stat;
+    	    		scenes[idx][0]=1;
+    	    		index++;
+    	    	}
+    	    	else{
+    	    		alert("SORRY! Database doesn't have the specified data!");
+    	    	}	
+    	    	scenelist();
+    			//document.getElementById("result").innerHTML=xmlhttp.responseText;
+    	    }
+    	    /*else{
+    	    	document.getElementById("result").innerHTML="readystate is " + xmlhttp.readyState + "  and status is " + xmlhttp.status + " CANNOT PROCEED";
+    	    }*/
+    	};
+    	
+    	xmlhttp.open("POST", "Query", true);
+    	//var param = "scene=haha";	
+    	var param = JSON.stringify(scene); //has to be serialized
+    	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    	//xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    	//xmlhttp.setRequestHeader("Content-length", param.length);
+    	//xmlhttp.setRequestHeader("Connection", "close");
+    	xmlhttp.send(param);
+
+    }
+    
+
+    function drawplot() {
+    	var scale = 5;
         var canvas = document.getElementById("plot");
         var context = canvas.getContext('2d');
+        context.clearRect ( 0 , 0 , canvas.width , canvas.height );
         context.beginPath();
         context.moveTo(100, 600);
         context.lineTo(100, 0);
@@ -137,31 +227,56 @@ function init() {
         context.moveTo(100, 600);
         context.lineTo(100 + 800, 600);
         context.stroke();
-        context.moveTo(200, 590);
-        context.lineTo(200, 600);
-        context.stroke();
-        context.fillText("scence1", 100 + 80, 610);
-        context.fillText("Mean", 200 + 10, 610 - 300 + 5);
-        context.moveTo(200 - 10, 610 - 300);
-        context.lineTo(200 + 10, 610 - 300);
-        context.stroke();
-        context.strokeRect(200 - 5, 610 - 300 - 20 / 2, 10, 20);
-        context.fillText("25%", 200 + 6, 610 - 300 - 30 + 5);
-        context.moveTo(200 - 6, 610 - 300 - 30);
-        context.lineTo(200 + 6, 610 - 300 - 30);
-        context.stroke();
-        context.fillText("75%", 200 + 4, 610 - 300 + 40 + 5);
-        context.moveTo(200 - 4, 610 - 300 + 40);
-        context.lineTo(200 + 4, 610 - 300 + 40);
-        context.stroke();
-        context.moveTo(200, 610 - 300 - 30);
-        context.lineTo(200, 610 - 300 + 40);
-        context.stroke();
-        context.moveTo(300, 590);
-        context.lineTo(300, 600);
-        context.stroke();
-        context.fillText("scence2", 100 + 180, 610);
-        context.fillText(index, 100 + 80+ index*100, 650);
+        
+        for(var i=0;i<(sceneno-1); i++){
+    		context.moveTo(100+100+100*i, 605);
+    		context.lineTo(100+100+100*i, 600);
+    		context.stroke();
+        }
+		context.moveTo(100+100+100*(sceneno-1), 595);
+		context.lineTo(100+100+100*(sceneno-1), 600);
+		context.stroke();
+        
+		
+		
+        var j=0;
+        for(var i=0;i<sceneno; i++){
+        	if( (scenes[i][0]==1) && (stats[i].valid==1)){
+        		//lengend for scenes, adjust the scene 
+        		context.fillText("scence"+j, 100+80+100*j-20, 620);
+        		context.moveTo(100+100+100*j, 605);
+        		context.lineTo(100+100+100*j, 600);
+        		context.stroke();
+        		//graph  20 is the offset, box width=20 
+        		context.fillText("Medium:"+stats[i].medium, 100+100+100*j+20, 600 - stats[i].medium*scale);
+        		context.moveTo(100+100+100*j - 10, 600 - stats[i].medium*scale);
+        		context.lineTo(100+100+100*j + 10, 600 - stats[i].medium*scale);
+        		context.stroke();
+        		//up-x, up-y, width, height
+        		context.strokeRect(100+100+100*j - 10, 600 - stats[i].p75*scale, 10*2, (stats[i].p75*scale-stats[i].p25*scale));
+        		//box top and bottom, offset the text in case overlapped
+        		context.fillText("75%:"+stats[i].p75, 100+100+100*j+20, 600 - stats[i].p75*scale-5);
+        		context.fillText("25%:"+stats[i].p25, 100+100+100*j+20, 600 - stats[i].p25*scale+5);
+        		//line top and bottom
+        		context.fillText("max:"+stats[i].max, 100+100+100*j  + 6, 600 - stats[i].max*scale -5);
+        		context.moveTo(100+100+100*j - 4, 600 - stats[i].max*scale);
+        		context.lineTo(100+100+100*j + 4, 600 - stats[i].max*scale);
+        		context.stroke();
+        		context.fillText("min:"+stats[i].min, 100+100+100*j + 6, 600 - stats[i].min*scale + 5);
+        		context.moveTo(100+100+100*j - 4, 600 - stats[i].min*scale);
+        		context.lineTo(100+100+100*j + 4, 600 - stats[i].min*scale);
+        		context.stroke();
+        		//vertical line
+        		context.moveTo(100+100+100*j, 600 - stats[i].max*scale);        		
+        		context.lineTo(100+100+100*j, 600 - stats[i].p75*scale);
+        		context.stroke();
+        		context.moveTo(100+100+100*j, 600 - stats[i].p25*scale);        		
+        		context.lineTo(100+100+100*j, 600 - stats[i].min*scale);
+        		context.stroke();
+         		j++;
+        	}
+
+        }
 
     }
 
@@ -176,4 +291,14 @@ function init() {
         context.moveTo(100, 600);
         context.lineTo(100 + 800, 600);
         context.stroke();
+        
+		context.fillText("scence axis", 100+100*sceneno/2, 650);   
+        for(var i=0;i<(sceneno-1); i++){
+    		context.moveTo(100+100+100*i, 605);
+    		context.lineTo(100+100+100*i, 600);
+    		context.stroke();
+        }
+		context.moveTo(100+100+100*(sceneno-1), 595);
+		context.lineTo(100+100+100*(sceneno-1), 600);
+		context.stroke();
     }
--- Flooring/src/edu/clarkson/env/Scene.java
+++ Flooring/src/edu/clarkson/env/Scene.java
@@ -9,7 +9,24 @@ package edu.clarkson.env;
  */
 public class Scene {
 	private String scheme;
+	private String house;
+	private String floor;
+	private String loading;
+	private String ventilation;
+	private String particle;
+	private String resuspension;
 
+	
+	@Override
+	public String toString() {
+		return "Scene [scheme=" + scheme + ", house=" + house + ", floor=" + floor + ", loading=" + loading 
+				+ ", ventilation=" + ventilation + ", particle=" + particle + ", resuspension=" + resuspension +",]";
+	}	
+	
+	public String toFile() {
+		return scheme + "_" + house + "_" + floor + "_" + loading 
+				+ "_" + ventilation + "_" + particle + "_" + resuspension +".csv";
+	}
 	/**
 	 * @return the scheme
 	 */
@@ -24,9 +41,89 @@ public class Scene {
 		this.scheme = scheme;
 	}
 	
-	@Override
-	public String toString() {
-		return "Scene [scheme=" + scheme + ",]";
+
+	/**
+	 * @return the house
+	 */
+	public String getHouse() {
+		return house;
+	}
+
+	/**
+	 * @param house the house to set
+	 */
+	public void setHouse(String house) {
+		this.house = house;
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public String getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+
+	/**
+	 * @return the loading
+	 */
+	public String getLoading() {
+		return loading;
+	}
+
+	/**
+	 * @param loading the loading to set
+	 */
+	public void setLoading(String loading) {
+		this.loading = loading;
+	}
+
+	/**
+	 * @return the ventilation
+	 */
+	public String getVentilation() {
+		return ventilation;
+	}
+
+	/**
+	 * @param ventilation the ventilation to set
+	 */
+	public void setVentilation(String ventilation) {
+		this.ventilation = ventilation;
+	}
+
+	/**
+	 * @return the particle
+	 */
+	public String getParticle() {
+		return particle;
+	}
+
+	/**
+	 * @param particle the particle to set
+	 */
+	public void setParticle(String particle) {
+		this.particle = particle;
+	}
+
+	/**
+	 * @return the resuspension
+	 */
+	public String getResuspension() {
+		return resuspension;
+	}
+
+	/**
+	 * @param resuspension the resuspension to set
+	 */
+	public void setResuspension(String resuspension) {
+		this.resuspension = resuspension;
 	}
 
 }
